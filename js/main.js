a1885180a9609e25b65d1359a11cbc02bc1fd84d
YubiFuu <jess.supercode@gmail.com>
2022-10-31 12:03:45 +0100

    small detail work

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const textInput = document.getElementById("input-num");
 const mwstBetrag = document.querySelector("h3");
 const endWort = document.querySelector(".brutto-o-netto");
 const endZahl = document.querySelector("h4");
+const textInputLabel = document.getElementById("label-input");
 
 function calcSteuer(event) {
     event.preventDefault();
@@ -11,17 +12,19 @@ function calcSteuer(event) {
     let x; //brutto oder netto
     let y; //steuersatz (7% oder 19%)
     let z; // die differenz zwischen textInput und x
+
     const defMwstSatz = () => {
-        if (mwst19.checked == true) {
+        if (mwst19.checked) {
             y = 19;
         } else {
             y = 7;
         }
     };
+
     defMwstSatz();
 
     const plusOrMinus = () => {
-        if (aufschlag.checked == true) {
+        if (aufschlag.checked) {
             x = numInput + (numInput / 100) * y;
             endWort.innerHTML = "Bruttobetrag (Endpreis)";
         } else {
@@ -37,6 +40,25 @@ function calcSteuer(event) {
     const difInputOutput = () => {
         z = Math.abs(Math.round((x - numInput) * 100) / 100);
     };
+
     difInputOutput();
     mwstBetrag.innerHTML = `${z} €`;
+}
+function changeNettoBruttoTag() {
+    if (aufschlag.checked == true) {
+        endWort.innerHTML = "Bruttobetrag (Endpreis)";
+        textInputLabel.innerHTML = `<p>
+        Nettobetrag (Preis ohne Mehrwertsteuer) in Euro
+        <span>*</span>
+    </p>`;
+    } else {
+        endWort.innerHTML = "Nettobetrag";
+        textInputLabel.innerHTML = `<p>
+        Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro
+        <span>*</span>
+    </p>`;
+    }
+    if (textInput.value != "") {
+        calcSteuer(event);
+    }
 }
